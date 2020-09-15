9a0d75f7daf7c92d58420e0a7ca8bcde2a71d9dd
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-09-15 01:08:07 -0500

    Create mood_api_call.py

---
+++ mood_api_call.py
@@ -0,0 +1,9 @@
+import chardet
+with open("spmooddata.csv", 'rb') as rawdata:
+    result = chardet.detect(rawdata.read(100000))
+result
+#{'encoding': 'Windows-1252', 'confidence': 0.73, 'language': ''}
+df= pd.read_csv(r"spmooddata.csv", encoding= "Windows-1252")
+songidcol= pd.DataFrame(df, columns= ["Song ID"])
+n= 50
+songidsub= [ songidcol[i:i+n] for i in range(0, len(songidcol), n) ]
