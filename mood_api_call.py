ffbafc7bb9cf96d55ace495ec841a8b99daf57d8
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-09-15 16:23:26 -0500

    Update mood_api_call.py

--- mood_api_call.py
+++ mood_api_call.py
@@ -1,11 +1,27 @@
 import chardet
 import csv
+import requests
+import json
 import pandas as pd
 with open("spmooddata.csv", 'rb') as rawdata:
     result = chardet.detect(rawdata.read(100000))
 result
 #{'encoding': 'Windows-1252', 'confidence': 0.73, 'language': ''}
 df= pd.read_csv(r"spmooddata.csv", encoding= "Windows-1252")
-songidcol= pd.DataFrame(df, columns= ["Song ID"])
-n= 50
-songidsub= [ songidcol[i:i+n] for i in range(0, len(songidcol), n) ]
+songidcol= df.to_json()
+json.dump(songidcol)
+songidparsed= songidcol["Song ID"]
+
+def get_concat_query(k=0, concatls=[]):
+    concatpre=[]
+    for i in range (0, len(songidparsed)):
+        while i<10+10*k:
+            songidval= songidparsed.get(str(i))
+            concatpre.append(songidval)
+        concatls.append(",".join(concatpre))
+        k=k+1
+        get_concat_query(k, concatls)
+            
+#songidcol= pd.DataFrame(df, columns= ["Song ID"])
+#n= 50
+#songidsub= [ songidcol[i:i+n] for i in range(0, len(songidcol), n) ]
